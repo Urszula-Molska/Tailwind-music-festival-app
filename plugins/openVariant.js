544d9ee0435615d08d527edea490bfa932de3af0
Urszula Molska <urszula_molska@wp.pl>
2023-08-11 23:22:44 +0200

    mobile menu

--- plugins/openVariant.js
+++ plugins/openVariant.js
@@ -2,6 +2,10 @@ const plugin = require("tailwindcss/plugin");
 
 const openVariant = plugin(function ({ addVariant }) {
   addVariant("group-open", ":merge(.group).open &");
+  addVariant("peer-open", ":merge(.peer).open ~ &");
 });
 
 module.exports = openVariant;
+
+// ~ is sibling selector
+// & is
